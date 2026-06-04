e01c79f8271316608da66a2c1f18399e895acbaa
10170236 <10170236@clases.edu.sv>
2026-06-04 08:59:16 -0600

    subiendo el estructura base

---
+++ Recursos-humanos/src/main/java/com/mycompany/recursos/humanos/RecursosHumanos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.recursos.humanos;
+
+/**
+ *
+ * @author carol
+ */
+public class RecursosHumanos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
